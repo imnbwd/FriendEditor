1445305fc85670e6cc1c42e5942f0f7a2140630b
Noah Yang <nbwd@qq.com>
2017-03-16 13:43:45 +0800

    Add project files.

---
+++ FriendEditor/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace FriendEditor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+        }
+    }
+}
